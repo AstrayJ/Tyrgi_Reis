d4479282c731b98a76e348cec500e9fc56eeb9a2
Da Vinci <jarljustus.hellat@real.edu.ee>
2024-05-08 09:41:25 +0300

    Links

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -25,8 +25,8 @@ export default function Welcome() {
                 <svg style={{position: "sticky"}} id="visual" viewBox="0 0 800 170" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="900" height="600" fill="#FA7268"></rect><path d="M0 173L21.5 178C43 183 86 193 128.8 194.2C171.7 195.3 214.3 187.7 257.2 187.2C300 186.7 343 193.3 385.8 192.7C428.7 192 471.3 184 514.2 177.5C557 171 600 166 642.8 173.3C685.7 180.7 728.3 200.3 771.2 204.3C814 208.3 857 196.7 878.5 190.8L900 185L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z" fill="#fa7268"></path><path d="M0 138L21.5 142.8C43 147.7 86 157.3 128.8 154.8C171.7 152.3 214.3 137.7 257.2 139.5C300 141.3 343 159.7 385.8 158.8C428.7 158 471.3 138 514.2 128.3C557 118.7 600 119.3 642.8 123.3C685.7 127.3 728.3 134.7 771.2 135.8C814 137 857 132 878.5 129.5L900 127L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z" fill="#ef5f67"></path><path d="M0 150L21.5 150.3C43 150.7 86 151.3 128.8 149.8C171.7 148.3 214.3 144.7 257.2 143.2C300 141.7 343 142.3 385.8 143.2C428.7 144 471.3 145 514.2 141C557 137 600 128 642.8 130.5C685.7 133 728.3 147 771.2 153.2C814 159.3 857 157.7 878.5 156.8L900 156L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z" fill="#e34c67"></path><path d="M0 110L21.5 106C43 102 86 94 128.8 89.3C171.7 84.7 214.3 83.3 257.2 84.5C300 85.7 343 89.3 385.8 87.7C428.7 86 471.3 79 514.2 75.8C557 72.7 600 73.3 642.8 75.7C685.7 78 728.3 82 771.2 85C814 88 857 90 878.5 91L900 92L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z" fill="#d53867"></path><path d="M0 37L21.5 39.3C43 41.7 86 46.3 128.8 45.2C171.7 44 214.3 37 257.2 33.5C300 30 343 30 385.8 34.7C428.7 39.3 471.3 48.7 514.2 48C557 47.3 600 36.7 642.8 38.8C685.7 41 728.3 56 771.2 63.7C814 71.3 857 71.7 878.5 71.8L900 72L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z" fill="#c62368"></path></svg>
                 <div className="riba">
                     <button className="nupp"> <a onClick={()=>window.location.href = route("meist")}> Meist</a></button>
-                    <button className="nupp">Ajakava</button>
-                    <button className="nupp">...</button>
+                    <button className="nupp"> <a onClick={()=>window.location.href = route("ajakava")}> Ajakava</a></button>
+                    <button className="nupp"> <a href = "https://drive.google.com/drive/folders/1-3ZvfWugqgpVN2TVP-QlIetGw2c9zCX_" target="_blank"> Pildid</a></button>
                     
                 </div>
                 
@@ -41,9 +41,9 @@ export default function Welcome() {
                     <div className="kirjeldus">
                         <h6 style={{fontSize: "40px", marginTop: "40px", marginBottom: "40px"}}>REISI KIRJELDUS</h6>
                         <ul style={{listStyleType: "circle", whiteSpace: "pre-wrap"}}>
-                            <li>Reisi kestvus: <strong>20.04 - 27.04</strong></li>
-                            <li></li>
-                            <li>dffsdf</li>
+                            <li><strong>Reisi kestvus: 20.04 - 27.04</strong></li>
+                            <li><strong>Kirjeldus</strong>: Ilmad olid mõnusalt soojad, päikest oli piisavalt, Atatürki nägi igalpool. </li>
+                            <li>Kuigi Türgi väga meeldis ei soovi sinna kunagi enam minna. </li>
                         </ul>
 
 
@@ -65,6 +65,7 @@ export default function Welcome() {
                             </div>
 
                             <div className="suvaline" style={{ position: "absolute", top: "20%", left: "40%", rotate: "-4deg" }}>
+                                {/* pane link pildi sisse: https://docs.google.com/spreadsheets/d/1XdUVOR9qAS5aHRarg4FrEuhEDWKNUBVpNRKVBbKYros/edit#gid=1916537345*/}
                                 <img className="madisepilt" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"></img>
                                 <h1 className="pilditekst2">Tõik keised</h1>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/meist', function () {
     return Inertia::render('Meist');
 })->name('meist');
 
+Route::get('/ajakava', function () {
+    return Inertia::render('Ajakava');
+})->name('ajakava');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
