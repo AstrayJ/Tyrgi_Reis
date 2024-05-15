63bc071d7ea4ffaca80f3070572de8a40e5f42ca
Da Vinci <jarljustus.hellat@real.edu.ee>
2024-05-15 08:41:34 +0300

    refactoring?

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,12 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('Welcome');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/meist', function () {
     return Inertia::render('Meist');
 });
 
-
-Route::get('/welcome', function () {
-    return Inertia::render('Welcome');
-})->name('Welcome');
-
-Route::get('/ajakava', function () {
-    return Inertia::render('Ajakava');
-})->name('ajakava');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
